f17625d4bc85b3dbd6a466391fb3f0b87209be23
ChristopherBanz <christopher.banz@gmail.com>
2022-04-29 18:51:58 -0400

    Figured out why the sorting was wonky

--- src/components/recordList.js
+++ src/components/recordList.js
@@ -77,10 +77,10 @@ export default function RecordList() {
             <table id="myTable2" className="table table-striped">
                 <thead>
                     <tr>
-                        <th onClick={() => sortTable(0)}>Name</th>
-                        <th onClick={() => sortTable(1)}>Score</th>
-                        <th onClick={() => sortTable(2)}>Highest Level</th>
-                        <th onClick={() => sortTable(3)}>Date</th>
+                        <th onClick={() => altSort(0)}>Name</th>
+                        <th onClick={() => altSort(1)}>Score</th>
+                        <th onClick={() => altSort(2)}>Highest Level</th>
+                        <th onClick={() => altSort(3)}>Date</th>
                     </tr>
                 </thead>
                 <tbody>{recordList()}</tbody>
@@ -88,63 +88,69 @@ export default function RecordList() {
         </div>
     );
 }
-    
-function sortTable(n) {
-    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+
+function altSort(n) {
+    var table, i, x, y;
     table = document.getElementById("myTable2");
-    // if (table == null){
-    //     console.log("its not there!");
-    // } else if (table != null){
-    //     console.log("its there alright");
-    // }
-    switching = true;
-    // Set the sorting direction to ascending:
-    dir = "asc";
-    /* Make a loop that will continue until
-    no switching has been done: */
+    var switching = true;
+    var direction = "ascending";
+    var count = 0;
+
+    console.log(table.rows.length)
     while (switching) {
-      // Start by saying: no switching is done:
-      switching = false;
-      rows = table.rows;
-      /* Loop through all table rows (except the
-      first, which contains table headers): */
-      for (i = 1; i < (rows.length - 1); i++) {
-        // Start by saying there should be no switching:
-        shouldSwitch = false;
-        /* Get the two elements you want to compare,
-        one from current row and one from the next: */
-        x = rows[i].getElementsByTagName("td")[n];
-        y = rows[i + 1].getElementsByTagName("td")[n];
-        /* Check if the two rows should switch place,
-        based on the direction, asc or desc: */
-        if (dir === "asc") {
-          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-            // If so, mark as a switch and break the loop:
-            shouldSwitch = true;
-            break;
-          }
-        } else if (dir === "desc") {
-          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-            // If so, mark as a switch and break the loop:
-            shouldSwitch = true;
-            break;
-          }
+        switching=false;
+        var rows = table.rows;
+
+        for(i = 1; i < (rows.length - 1); i++){
+            var Switch = false
+            x = rows[i].getElementsByTagName("td")[n].innerHTML;
+            y = rows[i + 1].getElementsByTagName("td")[n].innerHTML;
+            if (direction === "ascending"){
+                console.log("ascend to heaven!!");
+                if (Number(x) && Number(y)){
+                    parseInt(x);
+                    parseInt(y);
+                   if (parseInt(x) > parseInt(y)){
+                       Switch = true;
+                       break;
+                   }
+                } else{ 
+                    if (x.toLowerCase() > y.toLowerCase()){
+                        Switch = true;
+                        break;
+                    }
+                }
+            }
+            if (direction === "descending"){
+                console.log("descend to helllll");
+                if (Number(x) && Number(y)){
+                    parseInt(x);
+                    parseInt(y);
+                   if (parseInt(x) < parseInt(y)){
+                       Switch = true;
+                       break;
+                   }
+                } else{ 
+                    if (x.toLowerCase() < y.toLowerCase()){
+                        Switch = true;
+                        break;
+                    }
+                }
+            }             
         }
-      }
-      if (shouldSwitch) {
-        /* If a switch has been marked, make the switch
-        and mark that a switch has been done: */
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        switching = true;
-        // Each time a switch is done, increase this count by 1:
-        switchcount ++;
-      } else {
-        /* If no switching has been done AND the direction is "asc",
-        set the direction to "desc" and run the while loop again. */
-        if (switchcount === 0 && dir === "asc") {
-          dir = "desc";
-          switching = true;
+        if (Switch){
+            console.log('SWITCH!!!');
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i])
+            switching = true;
+            count ++;
+        } else {
+            console.log("swappa");
+            console.log(count);
+            console.log(direction);
+            if (count === 0 && direction === "ascending"){
+                direction = "descending";
+                switching = true;
+            }
         }
-      }
     }
-  }
+}
