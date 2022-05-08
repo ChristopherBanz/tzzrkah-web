671f7082a5db196c37806cc8ccc19c675fb09aa6
ChristopherBanz <christopher.banz@gmail.com>
2022-05-08 14:41:29 -0400

    i think i fixed it

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -15,25 +15,6 @@ export default function Navbar() {
                 <NavLink to="/">
                     <img src={logo} alt="Shooty2a"></img>
                 </NavLink>
-<<<<<<< HEAD
-=======
-                {/* <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button> */}
-              
-                <a href="https://dl.dropboxusercontent.com/s/au76v39blsvsv5c/Shooty2a-v0.1.32-setup.exe?dl=0">
-                
-                <button>DOWNLOAD SHOOTY2A Version 0.1.32</button>
-                </a>
->>>>>>> 70124ddeefa05a67eebb15f2f6900b78caeb91f3
 
                 <Box sx={{
             display:"flex",
